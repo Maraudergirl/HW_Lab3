ded9391cb6fa1bb0e7682f227286585f92088d3b
Camila Carrasco <ccarrasco.rivera.ks@gmail.com>
2017-10-30 18:57:03 -0300

    base apache con algunos avances, polinomio & triangulo html, css y js

---
+++ HW_Lab3/www/scripts/polinomio.js
@@ -0,0 +1,70 @@
+function solvePoli() {
+    var q, a;
+	var A, B, C;
+
+	var D;
+	var x1, x2, x_r, x1_i, x2_i;
+	var text1, text2;
+
+    q = document.getElementById("question");
+    a = document.getElementById("answer");
+    q.style.display = "none";
+    a.style.display = "block";
+
+    A = document.getElementById("coefA").value;
+    B = document.getElementById("coefB").value;
+    C = document.getElementById("coefC").value;
+
+	document.getElementById("eqA").innerHTML = A;
+	document.getElementById("eqB").innerHTML = B;
+	document.getElementById("eqC").innerHTML = C;
+
+	D = B*B - 4*A*C;
+
+	if (D > 0) {
+		x1 = (-B + Math.sqrt(D))/(2*A);
+		x2 = (-B - Math.sqrt(D))/(2*A);
+		document.getElementById("sol1").innerHTML = x1;
+		document.getElementById("sol2").innerHTML = x2;
+	}
+	else {
+		if (D < 0) {
+			x_r = - B/(2*A)
+			x1_i = Math.sqrt(-D)/(2*A);
+			x2_i = -Math.sqrt(-D)/(2*A);
+
+			/*
+			// Los siguientes if son para que se vea más bonito al escribir las soluciones
+			if (x1_r == 0){
+				if(x1_i < 0){
+					text1 = "- i·"+(x1_i*(-1));
+				}
+				else{
+					text1 = "i·"+x1_i;
+				}
+				if(x2_i < 0){
+					text2 = "- i·"+(x2_i*(-1));
+				}
+				else{
+					text2 = "i·"+x2_i;
+				}
+			}
+			else{
+				text1 = x_r + " + i·"+x1_i;
+				text2 = x_r + " + i·"+x2_i;
+			}
+			*/
+
+			text1 = x_r + "+ i·"+x1_i;
+			text2 = x_r + "+ i·"+x2_i;
+			document.getElementById("sol1").innerHTML = text1;
+			document.getElementById("sol2").innerHTML = text2;
+		}
+		else {
+			x1 = -B/(2*A);
+			x2 = x1;
+			document.getElementById("sol1").innerHTML = x1;
+			document.getElementById("sol2").innerHTML = x2;
+		}
+	}
+}
---
+++ HW_Lab3/www/scripts/triangulo.js
@@ -0,0 +1,65 @@
+var isTriangle = function(a, b, c){
+  if((a >= b + c)||(b >= a + c)||(c >= a + b)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+var orderSides = function(sides) {
+  return sides.sort(function(a,b) { return a - b }).reverse();
+};
+
+var drawTriangle = function(sides) {
+  var scale = 200/(sides[0]);
+  var peakX = (scale * sides[1] * Math.sin(Math.PI / 2 - Math.acos((Math.pow(sides[0],2) + Math.pow(sides[1],2) - Math.pow(sides[2],2)) / (2 * sides[0] * sides[1])))).toFixed(1);
+  var peakY = (scale * Math.sqrt(Math.pow(sides[1],2)-Math.pow(peakX / scale,2))).toFixed(1);
+  var colorA = "#39bda7";
+  var colorB = "#8f8f8f";
+  return '<svg height="' + peakY + '" width="400" id="canvas"><polygon points="0,0 200,0 ' + peakX + ',' + peakY + '" style="fill:' + colorA +';stroke:' + colorB +';stroke-width:1" /></svg>';
+};
+
+function solveTriLLL() {
+    var ques, ans;
+	var a, b, c;
+	var error;
+	var alpha, beta, gamma;
+
+
+    ques = document.getElementById("question");
+    ans = document.getElementById("answer");
+    ques.style.display = "none";
+    ans.style.display = "block";
+
+    a = document.getElementById("lado_a").value;
+    b = document.getElementById("lado_b").value;
+    c = document.getElementById("lado_c").value;
+
+	if (!isTriangle(a, b, c)){
+	    alpha = Math.acos((b*b + c*c - a*a)/(2*b*c)) // en radianes
+	    alpha = 180*alpha/Math.PI;	// en grados
+	    beta = Math.acos((a*a + c*c - b*b)/(2*a*c)) // en radianes
+	    beta = 180*beta/Math.PI;	// en grados
+	    gamma = 180 - alpha - beta;
+
+	    var triangle = {
+	    	side1: a,
+	    	side2: b,
+	    	side3: c
+	    };
+	    triangle['orderSides'] = function() {}
+	    //// DIBUJAR TRIANGULO
+
+		document.getElementById("triangle").innerHTML = drawTriangle(orderSides([triangle.side1,triangle.side2,triangle.side3]));
+	    //// Expresar VALORES OBTENIDOS
+
+		document.getElementById("alp").innerHTML = alpha;
+		document.getElementById("bet").innerHTML = beta;
+		document.getElementById("gam").innerHTML = gamma;
+
+    	}
+    else {
+    	//Mensaje de error
+    	alert("Los lados entregados no hacen un triángulo! Intenta otros valores");
+    }
+
+}
